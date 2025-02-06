4b97e398cbaf6183600b71d6e6bd6583180072a7
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-02-06 23:46:05 +0530

    index

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,27 @@
 import { Text, View } from "react-native";
+import { useState } from "react";
+import { DATA } from "@/data/todo";
 
 export default function Index() {
+  const [todos, setTodos] = useState(DATA.sort((a, b) => b.id - a.id));
+  const [text, setText] = useState('');
+
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{
+        id: newId,
+        title: text,
+        complete: false
+      }, ...todos]);
+      setText('');
+    }
+  }
+
+  const toggleTodo = (id: number) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, complete: !todo.complete } : todo));
+  }
+
   return (
     <View
       style={{
--- data/todo.ts
+++ data/todo.ts
@@ -2,11 +2,11 @@ export const DATA = [
     {
         'id': 1,
         'title': "lunch",
-        'complete': 'false'
+        'complete': false
     },
     {
         'id': 2,
         'title': "dinner",
-        'complete': 'true'
+        'complete': true
     },
 ];
